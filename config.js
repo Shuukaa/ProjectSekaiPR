dad301592cd37b6a305a540aba7344df71408e6d
Shuukaa <69614231+Shuukaa@users.noreply.github.com>
2025-05-03 03:40:42 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+const config = {
+    localStoragePrefix: "party-rank-sorter",
+    title: "Project Sekai Party Rank Sorter",
+    description: "Project Sekai Party rank sorter for your list of songs."
+};
