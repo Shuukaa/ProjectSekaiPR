b96e1139e5d1b525a6679a89abd4b3798b89a4f1
Shuukaa <69614231+Shuukaa@users.noreply.github.com>
2025-05-03 03:19:46 -0500

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-const config = {
-    localStoragePrefix: "party-rank-sorter",
-    title: "Project Sekai Party Rank Sorter",
-    description: "Project Sekai Party rank sorter for your list of songs."
-};
